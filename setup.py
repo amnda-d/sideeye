704c053e6286bfa132f730718fa4e2c66b1df69f
zediski <amandakdoucette@gmail.com>
2018-05-14 15:47:06 -0400

    add deploy to circleci, update version

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 from codecs import open
 from os import path
+
+VERSION = '1.0.0a2'
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 setup(
     name='sideeye',
-    version='1.0.0a1',
+    version=VERSION,
     description='Library for analyzing eye-tracking-while-reading data',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -44,5 +60,8 @@ setup(
 
     package_data={
         'sideeye': ['default_config.json'],
+    },
+    cmdclass={
+        'verify': VerifyVersionCommand,
     }
 )
