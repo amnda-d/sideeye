2a2a84a06fb565f5743a0993e57daae6c2df5132
zediski <amandakdoucette@gmail.com>
2018-05-14 16:03:16 -0400

    remove os

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class VerifyVersionCommand(install):
     description = 'verify that the git tag matches our version'
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG')
+        tag = getenv('CIRCLE_TAG')
 
         if tag != VERSION:
             info = "Git tag: {0} does not match the version of this app: {1}".format(
