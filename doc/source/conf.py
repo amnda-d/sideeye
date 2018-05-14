daa9bfdb26edd8eaaf20984e9e67267bf01ae041
zediski <amandakdoucette@gmail.com>
2018-05-14 12:24:42 -0400

    update sphinx extensions

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../sideeye'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.coverage', 'sphinxcontrib.napoleon']
+    'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
